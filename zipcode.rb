671dee053cc7f6651e3248ffa54c11c0d4022f61
Barrett Clark <bclark@geoforce.com>
2011-09-28 23:04:02 -0500

    Zipcode constructor with a test

--- zipcode.rb
+++ zipcode.rb
@@ -1,2 +1,13 @@
 class ZipCode
+  attr_accessor :zip, :city, :state, :latitude, :longitude, :timezone, :dst
+  
+  def initialize(attributes = {})
+    @zip = attributes[:zip]
+    @city = attributes[:city]
+    @state = attributes[:state]
+    @latitude = attributes[:latitude]
+    @longitude = attributes[:longitude]
+    @timezone = attributes[:timezone]
+    @dst = attributes[:dst]
+  end
 end
--- zipcode_test.rb
+++ zipcode_test.rb
@@ -1,4 +1,31 @@
 require 'test/unit'
+require File.expand_path(File.dirname(__FILE__), 'zipcode')
 
 class ZipCodeTest < Test::Unit::TestCase
+  # This runs before each test method
+  def setup
+    @zipcode = ZipCode.new(
+      :zip        => '00210',
+      :city       => 'Portsmouth',
+      :state      => 'NH',
+      :latitude   => '43.005895',
+      :longitude  => '-71.013202',
+      :timezone   => '-5',
+      :dst        => '1'
+    )
+  end
+  
+  # This runs after each test method
+  def teardown
+  end
+  
+  # You can have other supporting methods that the tests use
+  def supporting_method
+    puts 'This does nothing'
+  end
+  
+  # Methods that begin with 'test_' are evaluated by Test/Unit as tests
+  def test_initialize_hash
+    assert_equal '00210', @zipcode.zip
+  end
 end
